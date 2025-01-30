b977a1b7750bc155dfceb68de97e0f273839612f
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2025-01-30 13:30:55 +0200

    Update report_module_usage.tf

--- report_module_usage.tf
+++ report_module_usage.tf
@@ -43,14 +43,14 @@ variable "sleep_time" {
   default = 1
 }
 
-
+/*
 data "scalr_module_version" "report_get_modver_id" {
   count   = 21
   source  = "${scalr_environment.report_env[count.index].id}/${var.module_name}/${var.module_provider}"
   version = var.module_existing_version_number
   depends_on = [ null_resource.wait_for_module]
 }
-
+*/
 resource "scalr_workspace" "report_ws" {
   count = 21
   environment_id = scalr_environment.report_env[count.index].id
