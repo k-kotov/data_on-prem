a9fec1fa466939b48d82881fa87ce5cb9f1b2090
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2025-09-04 11:43:02 +0300

    Update policy_group.tf

--- policy_group.tf
+++ policy_group.tf
@@ -1,7 +1,7 @@
 resource "scalr_policy_group" "policy" {
   name            = "auto_policy_${formatdate("DDMMYYYY", timestamp())}"
   opa_version     = var.opa_version
-  vcs_provider_id = scalr_vcs_provider.vcs.id
+  vcs_provider_id = "vcs-v0ouva2scjtc0blfh"
   vcs_repo {
     identifier = var.policy_vcs-repo_identifier
     path       = var.policy_vcs-repo_path
@@ -12,4 +12,5 @@ resource "scalr_policy_group" "policy" {
 resource "scalr_policy_group_linkage" "policy_link" {
   policy_group_id = scalr_policy_group.policy.id
   environment_id  = scalr_environment.env_no_tag.id
+
 }
