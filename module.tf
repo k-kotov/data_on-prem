4aeb3629faed0a30ba33bcd04e9647eb07a20125
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2025-09-05 10:54:52 +0300

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,10 +1,11 @@
 resource "scalr_module" "module" {
   environment_id  = scalr_environment.env_no_tag.id
-  vcs_provider_id = "vcs-v0ouva2scjtc0blfh"
+  vcs_provider_id = "vcs-v0ov0h9hjod4de9jr"
   vcs_repo {
     identifier = var.module_vcs-repo_identifier
     path       = var.module_vcs-repo_path
     #tag_prefix = var.module_vcs-repo_tag-prefix
   }
 }
+
 
